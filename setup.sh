84939547808a6aa37e5b3855889f18c0669fff62
MatthewJohn <matthew@dockstudios.co.uk>
2019-01-07 19:29:35 +0000

    Add initial notes

---
+++ setup.sh
@@ -0,0 +1,347 @@
+export MYSQL_USER=root
+export MYSQL_PASSWORD=
+export BASE_DOMAIN=ff.dockstudios.co.uk
+
+
+
+apt-get update
+apt install curl nodejs npm git postfix memcached redis mysql-server graphicsmagick --assume-yes
+npm install -g grunt-cli grunt
+
+# Install Rust
+curl https://sh.rustup.rs -sSf | sh
+
+
+cd /
+git clone https://github.com/mozilla-services/pushbox
+cd pushbox
+
+cat > /pushbox/Rocket.toml <<EOF
+[production]
+## Database DSN URL.
+database_url="mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@localhost/pushbox"
+## used by FxA OAuth token authorization.
+fxa_host="oauth.${BASE_DOMAIN}"
+## set "dryrun" to "true" to skip ANY authorization checks.
+#dryrun=false
+## used by the FXA Server key authorization
+server_token="changeme"
+[global.limits]
+# Maximum accepted data size for JSON payloads.
+json = 1048576
+
+EOF
+
+
+cd /
+git clone git://github.com/mozilla/fxa-auth-server.git
+cd /fxa-auth-server
+npm install
+NODE_ENV=prod node ./scripts/gen_keys.js
+NODE_ENV=prod node ./fxa-oauth-server/scripts/gen_keys.js
+NODE_ENV=prod node ./scripts/gen_vapid_keys.js
+
+cd /
+git clone https://github.com/mozilla/fxa-content-server
+cd /fxa-content-server
+npm install --production
+npm install bluebird
+npm run build-production
+#grunt install
+#grunt server:dist
+
+cd /
+git clone https://github.com/mozilla/fxa-auth-db-mysql
+cd /fxa-auth-db-mysql
+npm install
+node bin/db_patcher.js
+
+cd /
+git clone https://github.com/mozilla/fxa-profile-server
+cd fxa-profile-server
+npm install
+sed -i '/process.env.NODE_ENV/d' scripts/run_dev.js
+sed -i "s/throw new Error('config.events must be included in prod');/logger.warn('');/g" lib/events.js
+
+
+volumes /var/lib/mysql
+
+
+cat > /fxa-profile-server/config/production.json <<EOF
+{
+  "env": "production",
+  "logging": {
+    "fmt": "pretty",
+    "level": "all",
+    "debug": true
+  },
+  "db": {
+    "driver": "mysql"
+  },
+  "img": {
+    "driver": "local"
+  },
+  "customsUrl": "https://profile.${BASE_DOMAIN}/a/{id}",
+  "serverCache": {
+    "useRedis": true
+  },
+  "authServer": {
+    "url":"https://auth.${BASE_DOMAIN}:9000/v1"
+  },
+  "mysql":{
+     "createSchema":true,
+     "user":"${MYSQL_USER}",
+     "password":"${MYSQL_PASSWORD}",
+     "database":"fxa_profile",
+     "host":"localhost",
+     "port":"3306"
+  },
+  "oauth":{
+     "url":"https://oauth.${BASE_DOMAIN}/v1"
+  },
+  "publicUrl":"https://profile.${BASE_DOMAIN}",
+  "server":{
+     "host":"0.0.0.0",
+     "port":1111
+  },
+  "worker":{
+     "host":"127.0.0.1",
+     "port":1113,
+     "url":"http://127.0.0.1:1113"
+  },
+  "serverCache":{
+     "redis":{
+        "host":"127.0.0.1",
+        "keyPrefix":"fxa-profile",
+        "port":6379
+     },
+     "useRedis":true,
+     "expiresIn":3600000,
+     "generateTimeout":11000
+  }
+}
+EOF
+
+cat > /fxa-auth-db-mysql/config/index.js <<EOF
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+var fs = require('fs')
+var path = require('path')
+var url = require('url')
+var convict = require('convict')
+
+module.exports = require('./config')(fs, path, url, convict)
+root@5e663df9cabf:/fxa-auth-db-mysql# cat config/config.js
+
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+module.exports = function (fs, path, url, convict) {
+
+  var conf = convict({
+    env: {
+      doc: 'The current node.js environment',
+      default: 'prod',
+      format: [ 'dev', 'test', 'stage', 'prod' ],
+      env: 'NODE_ENV',
+    },
+    hostname: {
+      doc: 'The IP address the server should bind to',
+      default: '0.0.0.0',
+      env: 'HOST',
+    },
+    port: {
+      doc: 'The port the server should bind to',
+      default: 8000,
+      format: 'port',
+      env: 'PORT',
+    },
+    logging: {
+      app: {
+        default: 'fxa-auth-db-server'
+      },
+      fmt: {
+        format: ['heka', 'pretty'],
+        default: 'heka'
+      },
+      level: {
+        env: 'LOG_LEVEL',
+        default: 'info'
+      },
+      uncaught: {
+        format: ['exit', 'log', 'ignore'],
+        default: 'exit'
+      }
+    },
+    patchKey: {
+      doc: 'The name of the row in the dbMetadata table which stores the patch level',
+      default: 'schema-patch-level',
+      env: 'SCHEMA_PATCH_KEY',
+    },
+    enablePruning: {
+      doc: 'Enables (true) or disables (false) pruning',
+      default: false,
+      format: Boolean,
+      env: 'ENABLE_PRUNING',
+    },
+    pruneEvery: {
+      doc: 'Approximate time between prunes (in ms)',
+      default: '1 hour',
+      format: 'duration',
+      env: 'PRUNE_EVERY',
+    },
+    pruneTokensMaxAge: {
+      // This setting must always be older than token lifetimes in the fxa-auth-server
+      doc: 'Time after which to prune account, password and unblock tokens (in ms)',
+      default: '3 months',
+      format: 'duration',
+      env: 'PRUNE_TOKENS_MAX_AGE',
+    },
+    signinCodesMaxAge: {
+      doc: 'Maximum age for signinCodes, after which they will expire',
+      default: '2 days',
+      format: 'duration',
+      env: 'SIGNIN_CODES_MAX_AGE',
+    },
+    requiredSQLModes: {
+      doc: 'Comma-separated list of SQL mode flags to enforce on each connection',
+      default: '',
+      format: 'String',
+      env: 'REQUIRED_SQL_MODES',
+    },
+    master: {
+      user: {
+        doc: 'The user to connect to for MySql',
+        default: 'root',
+        env: 'MYSQL_USER',
+      },
+      password: {
+        doc: 'The password to connect to for MySql',
+        default: '',
+        env: 'MYSQL_PASSWORD',
+      },
+      host: {
+        doc: 'The host to connect to for MySql',
+        default: 'localhost',
+        env: 'MYSQL_HOST',
+      },
+      port: {
+        doc: 'The port to connect to for MySql',
+        default: 3306,
+        format: 'port',
+        env: 'MYSQL_PORT',
+      },
+      connectionLimit: {
+        doc: 'The maximum number of connections to create at once.',
+        default: 10,
+        format: 'nat',
+        env: 'MYSQL_CONNECTION_LIMIT',
+      },
+      waitForConnections: {
+        doc: "Determines the pool's action when no connections are available and the limit has been reached.",
+        default: true,
+        format: Boolean,
+        env: 'MYSQL_WAIT_FOR_CONNECTIONS',
+      },
+      queueLimit: {
+        doc: "Determines the maximum size of the pool's waiting-for-connections queue.",
+        default: 100,
+        format: 'nat',
+        env: 'MYSQL_QUEUE_LIMIT',
+      },
+    },
+    slave: {
+      user: {
+        doc: 'The user to connect to for MySql',
+        default: 'root',
+        env: 'MYSQL_SLAVE_USER',
+      },
+      password: {
+        doc: 'The password to connect to for MySql',
+        default: '',
+        env: 'MYSQL_SLAVE_PASSWORD',
+      },
+      host: {
+        doc: 'The host to connect to for MySql',
+        default: '127.0.0.1',
+        env: 'MYSQL_SLAVE_HOST',
+      },
+      port: {
+        doc: 'The port to connect to for MySql',
+        default: 3306,
+        format: 'port',
+        env: 'MYSQL_SLAVE_PORT',
+      },
+      connectionLimit: {
+        doc: 'The maximum number of connections to create at once.',
+        default: 10,
+        format: 'nat',
+        env: 'MYSQL_SLAVE_CONNECTION_LIMIT',
+      },
+      waitForConnections: {
+        doc: "Determines the pool's action when no connections are available and the limit has been reached.",
+        default: true,
+        format: Boolean,
+        env: 'MYSQL_SLAVE_WAIT_FOR_CONNECTIONS',
+      },
+      queueLimit: {
+        doc: "Determines the maximum size of the pool's waiting-for-connections queue.",
+        default: 100,
+        format: 'nat',
+        env: 'MYSQL_SLAVE_QUEUE_LIMIT',
+      },
+    },
+    ipHmacKey: {
+      doc: 'A secret to hash IP addresses for security history events',
+      default: 'changeme',
+      env: 'IP_HMAC_KEY'
+    },
+    sentryDsn: {
+      doc: 'Sentry DSN for error and log reporting',
+      default: '',
+      format: 'String',
+      env: 'SENTRY_DSN'
+    },
+    recoveryCodes: {
+      length: {
+        doc: 'The length of a recovery code',
+        default: 10,
+        format: 'nat',
+        env: 'RECOVERY_CODE_LENGTH'
+      }
+    }
+  })
+
+  // handle configuration files. you can specify a CSV list of configuration
+  // files to process, which will be overlayed in order, in the CONFIG_FILES
+  // environment variable. By default, the ./config/<env>.json file is loaded.
+
+  var envConfig = path.join(__dirname, conf.get('env') + '.json')
+  envConfig = envConfig + ',' + process.env.CONFIG_FILES
+
+  var files = envConfig.split(',').filter(fs.existsSync)
+  conf.loadFile(files)
+  conf.validate({ allowed: 'strict' })
+
+  return conf.getProperties()
+}
+EOF
+
+docker cp  /fxa-auth-server/config/index.js
+docker cp /fxa-content-server/server/config/local.json
+docker cp /fxa-auth-db-mysql/config/config.js
+
+
+service mysql start
+echo "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY ''; FLUSH PRIVILEGES;" | mysql
+service memcached start
+redis-server &
+
+pushd /fxa-auth-server; NODE_ENV=prod scripts/start-server.sh &; popd
+
+pushd /fxa-content-server; NODE_ENV=production npm start &; popd
+pushd /fxa-auth-db-mysql; NODE_ENV=prod npm start &; popd
+pushd /fxa-profile-server; NODE_ENV=prod npm start &; popd
